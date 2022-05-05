e78a95d5f711d3e63f97b9dddbbaeef6a9a40256
Piyush Kumar <20BCS9107@cuchd.in>
2022-05-05 20:08:13 +0530

    Create 1-basic-array.cpp

---
+++ 6-Array-Core/1-basic-array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int A[n];
+    for (int i = 0; i<n; i++){
+        cin>>A[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    
+    return 0;
+}
