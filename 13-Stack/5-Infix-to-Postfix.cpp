09d2ec9d6f94051e3c6645496dbea2f24c9edab4
Piyush Kumar <20BCS9107@cuchd.in>
2022-06-20 13:50:56 +0530

    Conversion using Algorithm

--- 13-Stack/5-Infix-to-Postfix.cpp
+++ 13-Stack/5-Infix-to-Postfix.cpp
@@ -88,18 +88,39 @@ int stackTop()
 }
 
 
-int pre(char x)
+int outPrecedence(char x)
 {
     if(x == '+' || x == '-')
     return 1;
     else if(x == '*' || x == '/')
-    return 2;
+    return 3;
+    else if(x == '^')
+    return 6;
+    else if(x == '(')
+    return 7;
+    else if(x == ')')
     return 0;
+
+    return -1;
+}
+
+int inPrecedence(char x)
+{
+    if(x == '+' || x == '-')
+    return 2;
+    else if(x == '*' || x == '/')
+    return 4;
+    else if(x == '^')    // R to L associativity
+    return 5;
+    else if(x == '(')
+    return 0;
+
+    return -1;
 }
 
 int isOperand(char x)
 {
-    if(x == '+' || x == '-' || x == '*' || x == '/')
+    if(x == '+' || x == '-' || x == '*' || x == '/' || x == '^' || x == '(' || x == ')')
     return 0;
     else 
     return 1;
@@ -116,10 +137,17 @@ char *infixToPostfix(char *infix)
         postfix[j++] = infix[i++];
         else
         {
-            if(pre(infix[i]) > pre(stackTop()))
+            if(stackTop() == -1 || outPrecedence(infix[i]) > inPrecedence(stackTop()))
             push(infix[i++]);
             else
-            postfix[j++] = pop();
+            {
+                if(outPrecedence(infix[i]) == inPrecedence(stackTop())){
+                    pop();
+                    i++;
+                }
+                else
+                postfix[j++] = pop();
+            }   
         }
     }
     while(!isEmpty())
@@ -138,9 +166,11 @@ int main(){
 
     char* infix = "a+b*c";
     char* infix1 = "a+b*c-d/e";
+    char* infix2 = "((a+(b*c))-d)";
+    char* infix3 = "(a+b)*c-d^e^f";
     // push(1);
 
-    char* postfix = infixToPostfix(infix1);
+    char* postfix = infixToPostfix(infix3);
 
     cout<<postfix;
 
