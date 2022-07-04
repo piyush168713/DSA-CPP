7beba58360b0967ff8acafbc4bca866ec3c065d8
Piyush Kumar <20BCS9107@cuchd.in>
2022-07-04 12:41:49 +0530

    Implemented Queue using STL

--- 14-Queue/4-queue-stl.cpp
+++ 14-Queue/4-queue-stl.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+#include<queue>
+using namespace std;
 
+int main(){
+
+    queue<int> q;
+
+    q.push(5);
+    cout<<"Front of Queue is: "<<q.front()<<endl;
+    q.push(10);
+    cout<<"Front of Queue is: "<<q.front()<<endl;
+    q.push(15);
+    q.push(20);
+    cout<<"Size of queue is: "<<q.size()<<endl;
+
+    q.pop();
+    cout<<"Size of queue is: "<<q.size()<<endl;
+
+    if(q.empty())
+    cout<<"Queue is Empty"<<endl;
+    else
+    cout<<"Queue is not empty"<<endl;
+    
+    return 0;
+} 
