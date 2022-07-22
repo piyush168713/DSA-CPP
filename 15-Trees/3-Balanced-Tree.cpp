3b02b34b8202dfd461c5848a492b1db6d04b2c5b
Piyush Kumar <20BCS9107@cuchd.in>
2022-07-22 15:52:28 +0530

    GFG

---
+++ 15-Trees/3-Balanced-Tree.cpp
@@ -0,0 +1,85 @@
+/*
+Problem Statement
+
+Check for Balanced Tree - GFG
+
+Given a binary tree, find if it is height balanced or not. 
+A tree is height balanced if difference between heights of left and right subtrees is not more than one for all nodes of tree. 
+
+A height balanced tree
+        1
+     /     \
+   10      39
+  /
+5
+
+An unbalanced tree
+        1
+     /    
+   10   
+  /
+5
+
+Example 1:
+
+Input:
+      1
+    /
+   2
+    \
+     3 
+Output: 0
+Explanation: The max difference in height
+of left subtree and right subtree is 2,
+which is greater than 1. Hence unbalanced
+
+Your Task:
+You don't need to take input. Just complete the function isBalanced() that takes root node as parameter and returns true, 
+if the tree is balanced else returns false.
+
+Constraints:
+1 <= Number of nodes <= 105
+0 <= Data of a node <= 106
+
+Expected time complexity: O(N)
+Expected auxiliary space: O(h) , where h = height of tree
+
+*/
+
+// Code
+
+//Function to check whether a binary tree is balanced or not.
+    pair<bool,int> isBalancedFast(Node* root)
+    {
+        if(root == NULL)
+        {
+            pair<bool, int> p = make_pair(true,0); 
+            return p;
+        }
+        
+        pair<int,int> left = isBalancedFast(root->left);
+        pair<int,int> right = isBalancedFast(root->right);
+        
+        
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+        
+        bool diff = abs(left.second - right.second) <= 1;
+        
+        pair<bool, int> ans;
+        ans.second = max(left.second, right.second) + 1;
+        
+        if(leftAns && rightAns && diff){
+            ans.first = true;
+        }
+        else{
+            ans.first = false;
+        }
+        return ans;
+    }
+    
+    bool isBalanced(Node *root)
+    {
+        //  Your Code here
+        return isBalancedFast(root).first;
+    }
