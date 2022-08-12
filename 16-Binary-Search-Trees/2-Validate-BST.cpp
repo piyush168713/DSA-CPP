cf73b3de851225d200b26a5ffd3edd3dac7df6a6
Piyush Kumar <20BCS9107@cuchd.in>
2022-08-12 15:41:44 +0530

    CodeStudio

---
+++ 16-Binary-Search-Trees/2-Validate-BST.cpp
@@ -0,0 +1,26 @@
+// Problem Statement
+
+// Validate BST - CodeStudio
+
+// Link - https://www.codingninjas.com/codestudio/problems/validate-bst_981275
+
+bool isBst(BinaryTreeNode<int>* root, int min, int max)
+{
+    if(root == NULL){
+        return true;
+    }
+    
+    if(root->data > min && root->data < max){
+        bool left = isBst(root->left, min, root->data);
+        bool right = isBst(root->right, root->data, max);
+        return left && right;
+    }
+    else
+    return false;
+}
+
+bool validateBST(BinaryTreeNode<int>* root) 
+{
+    // Write your code here
+    return isBst(root, INT_MIN, INT_MAX);
+}
