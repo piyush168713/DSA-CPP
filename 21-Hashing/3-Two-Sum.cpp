813e6182849e77b917e4bae833a843b8f80d4145
Piyush Kumar <20BCS9107@cuchd.in>
2022-09-19 09:55:41 +0530

    codeStudio/Leetcode

--- 21-Hashing/3-Two-Sum.cpp
+++ 21-Hashing/3-Two-Sum.cpp
@@ -30,3 +30,27 @@ vector<pair<int,int>> twoSum(vector<int>& arr, int target, int n)
     }
     return ans;
 }
+
+
+// Leetcode
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;                  
+        unordered_map<int,int>m;
+        
+        for(int i=0;i<nums.size();i++)    
+        {
+            int val = target-nums[i];
+            if(m.find(val)!=m.end()) 
+            {
+                ans.push_back(m.find(val)->second);
+                ans.push_back(i);
+                break;
+            }
+            m.insert(pair<int,int>(nums[i],i)); 
+        }
+        return ans;
+    }
+};
