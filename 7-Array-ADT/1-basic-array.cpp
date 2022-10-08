b61e98a63ba0ddbdecbcf61abeae20e0ad942dc0
Muskan kumari <86469435+Muskankumari2002@users.noreply.github.com>
2022-10-08 15:03:53 +0530

    here I simply used loop and cout to display the element in an array, also here I am taking inputs in array from user.

--- 7-Array-ADT/1-basic-array.cpp
+++ 7-Array-ADT/1-basic-array.cpp
@@ -1,27 +1,48 @@
+// #include<iostream>
+// using namespace std;
+
+// class Array{
+//     public:
+//     int A[20];
+//     int size;
+//     int length;
+// };
+
+// void Display(Array arr){
+//     cout<<"Elements are: ";
+//     for (int i = 0; i < arr.length; i++)
+//     {
+//         cout<<arr.A[i]<<" ";
+//     }
+    
+// }
+
+// int main(){
+
+//     Array arr = {{2,3,4,5,6}, 20, 5};
+
+//     Display(arr);   
+    
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
-
-class Array{
-    public:
-    int A[20];
+int main()
+{
     int size;
-    int length;
-};
-
-void Display(Array arr){
-    cout<<"Elements are: ";
-    for (int i = 0; i < arr.length; i++)
+    int a[100];
+    cout<<"Enter the size of an array"<<endl;
+    cin>>size;
+    cout<<"Enter the element in an array"<<endl;
+     for(int i=0;i<size;i++)
     {
-        cout<<arr.A[i]<<" ";
+        cin>>a[i];
     }
-    
-}
-
-int main(){
-
-    Array arr = {{2,3,4,5,6}, 20, 5};
-
-    Display(arr);   
-    
+    cout<<"Display the elements in an array"<<endl;
+    for(int i=0;i<size;i++)
+    {
+        cout<<a[i];
+    }
     return 0;
 }
