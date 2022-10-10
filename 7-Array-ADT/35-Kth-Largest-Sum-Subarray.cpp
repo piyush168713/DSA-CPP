49e315905b9ea889529626cdcdd0f58989f84a31
Piyush Kumar <20BCS9107@cuchd.in>
2022-10-10 10:47:01 +0530

    Create 35-Kth-Largest-Sum-Subarray.cpp

---
+++ 7-Array-ADT/35-Kth-Largest-Sum-Subarray.cpp
@@ -0,0 +1,31 @@
+#include<algorithm>
+#include<queue>
+int getKthLargest(vector<int> &arr, int k)
+{
+	//	Write your code here.    
+    // S.C - O(K)
+    int n = arr.size();
+    // creating min heap using priority queue (Min Heap)
+    priority_queue<int, vector<int>, greater<int>> mini;
+    
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+        for(int j = i; j < n; j++){
+            sum += arr[j];
+            
+            if(mini.size() <= k)
+            {
+                mini.push_back(sum);
+            }
+            else
+            {
+                if(sum >= mini.top())
+                {
+                    mini.pop();
+                    mini.push(sum);
+                }
+            }
+        }
+    }
+    return mini.top();
+}
